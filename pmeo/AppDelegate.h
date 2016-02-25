1f79470dbc8b3a1008197d48b6f3937af89b0e88
苏乾坤 <suqiankun@hotmail.com>
2016-02-25 17:03:19 +0800

    Initial Commit

---
+++ pmeo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pmeo
+//
+//  Created by 两好三坏 on 16/2/25.
+//  Copyright © 2016年 qinakun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pmeo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pmeo
+//
+//  Created by 两好三坏 on 16/2/25.
+//  Copyright © 2016年 qinakun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
